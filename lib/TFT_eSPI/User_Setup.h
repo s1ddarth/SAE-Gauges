0ec885d39a7561414a13530bbbdd31c490e7a392
Siddarth Menon <siddarthmenonk@gmail.com>
2025-11-20 20:14:54 -0600

    Updating pins and SPI frequency in user_setup.h

--- lib/TFT_eSPI/User_Setup.h
+++ lib/TFT_eSPI/User_Setup.h
@@ -1,22 +1,15 @@
-// ************ TFT_eSPI User Setup ************
-
-// Select the driver
 #define GC9A01_DRIVER
 
-// Display resolution
 #define TFT_WIDTH  240
 #define TFT_HEIGHT 240
 
-// SPI pins
-#define TFT_SCLK 36
-#define TFT_MOSI 35
+#define TFT_SCLK 12
+#define TFT_MOSI 11
 #define TFT_CS   10
 #define TFT_DC    9
 #define TFT_RST   8
 
-// Optional settings
-#define SPI_FREQUENCY  40000000  // 40 MHz SPI
+#define SPI_FREQUENCY  10000000
 #define TFT_RGB_ORDER TFT_RGB
 
-// Touch is not used
 #define TOUCH_CS -1
