998e11b6248066e70a696e8b318872d697688630
Siddarth Menon <siddarthmenonk@gmail.com>
2026-02-12 10:10:35 -0600

    Updated code for our new board

--- src/speedometer.ino
+++ src/speedometer.ino
@@ -1,44 +1,17 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 
-#define PIN_NEOPIXEL 48
-#define NUM_PIXELS 1
-
-Adafruit_NeoPixel pixels(NUM_PIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+#define ONBOARD_LED 38
+// Onboard LED is GPIO 38
 
 void setup(){
   Serial.begin(115200);
   delay(1000);
   
-  pixels.begin();
-  pixels.show();
-  
   Serial.println("STARTING");
-  Serial.println("LED initialized");
 }
 
 void loop() {
-  // Red
-  pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-  pixels.show();
-  Serial.println("LED: Red");
-  delay(1000);
-  
-  // Green
-  pixels.setPixelColor(0, pixels.Color(0, 255, 0));
-  pixels.show();
-  Serial.println("LED: Green");
-  delay(1000);
-  
-  // Blue
-  pixels.setPixelColor(0, pixels.Color(0, 0, 255));
-  pixels.show();
-  Serial.println("LED: Blue");
-  delay(1000);
-  
-  // Off
-  pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-  pixels.show();
-  Serial.println("LED: Off");
+  int i = 1;
+  Serial.println("Looping: " + i);
   delay(1000);
 }
